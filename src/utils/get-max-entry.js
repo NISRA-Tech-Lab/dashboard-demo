628bf8a00fe11ebf5ca45631a398adbdec7be74c
brianq-nisra <brian.quinn@nisra.gov.uk>
2026-06-02 13:06:57 +0100

    horizontal bar chart

--- src/utils/get-max-entry.js
+++ src/utils/get-max-entry.js
@@ -1,7 +1,10 @@
-export const getMaxEntry = (obj) => {
+export const getMaxEntry = (obj, type = "max") => {
     const entries = Object.entries(obj);
-    const maxEntry = entries.reduce((max, current) => 
-        current[1] > max[1] ? current : max
-    );
-    return { key: maxEntry[0], value: maxEntry[1] };
+    const entry = entries.reduce((best, current) => {
+        const compare = type === "min"
+            ? current[1] < best[1]
+            : current[1] > best[1];
+        return compare ? current : best;
+    });
+    return { key: entry[0], value: entry[1] };
 };
