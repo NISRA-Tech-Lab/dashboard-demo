e5b19405916793c22fcf64e2093d10372f7736a6
brianq-nisra <brian.quinn@nisra.gov.uk>
2026-08-14 15:35:53 +0100

    launch script update

--- src/r/launch-ui.R
+++ src/r/launch-ui.R
@@ -1,6 +1,44 @@
-if (!require(dashboardBuildR)) {
-  if (!require(remotes)) install.packages("remotes")
-  remotes::install_github("NISRA-Tech-Lab/dashboard-buildr")
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+
+try(
+  remotes::install_github(
+    "NISRA-Tech-Lab/dashboard-buildr",
+    upgrade = "never",
+    quiet = TRUE
+  ),
+  silent = TRUE
+)
+
+if (!requireNamespace("dashboardBuildR", quietly = TRUE)) {
+  stop(
+    paste(
+      "Dashboard BuildR is not installed and could not be",
+      "downloaded from GitHub. Check your internet connection",
+      "and try again."
+    )
+  )
+}
+
+running_in_rstudio <- identical(
+  Sys.getenv("RSTUDIO"),
+  "1"
+)
+
+if (running_in_rstudio) {
+
+  cat(
+    "\n\nLaunching NISRA Dashboard BuildR in Browser...\n\n",
+    "Press Esc to quit\n"
+  )
+
+} else {
+
+  cat(
+    "\n\nLaunching NISRA Dashboard BuildR in Browser...\n\n",
+    "Press Ctrl + C to quit\n"
+  )
 }
 
 dashboardBuildR::run_dashboard_buildr()
